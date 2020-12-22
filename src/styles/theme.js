19b56d11d650721887782769ddd617589a7f18a7
Victor Nnaji <nnajivictor0@gmail.com>
2020-12-22 17:24:28 +0100

    set up favicons and themes

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+    colors: {
+        primary : '#111112',
+        secondary: '#ff6161'
+    },
+    fonts: {
+        Nunito : 'Nunito, sans-serif',
+    }
+}
+
+export default theme;
